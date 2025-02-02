927530f8e69d5621860384f5ec2af023744ab411
abhishekojha752586 <101664033+abhishekojha752586@users.noreply.github.com>
2025-02-02 17:45:42 +0530

    Update WhatsAppIcon.tsx

--- src/app/components/WhatsAppIcon.tsx
+++ src/app/components/WhatsAppIcon.tsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa";
 const WhatsAppIcon: React.FC = () => {
   return (
     <a
-      href="https://wa.me/7525899459"
+      href="https://wa.me/7522899459"
       className="fixed bottom-6 right-6 bg-green-500 text-white rounded-full p-3 flex items-center justify-center shadow-lg animate-bounce"
       target="_blank"
       rel="noopener noreferrer"
